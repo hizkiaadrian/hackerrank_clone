a63731236635d95928ca8c9619afe5cb7ce5c69d
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-12-05 13:37:05 +0000

    Initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <div className='top-nav'>Hello</div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{backgroundColor: 'white', width: '50%'}}>
+        <h1>Question</h1>
+      </div>
+      <div style={{backgroundColor: 'grey', width:'50%'}}>
+        <h1>Editor</h1>
+      </div>
     </div>
+    <div className="footer">OSGD</div>
+    </>
   );
 }
 
