3f14313d832439b91e06dc08eb345d3892e12b33
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-12-06 15:19:21 +0000

    Pass question parameters as props

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,26 @@ function App() {
     <div className='top-nav'><h3 style={{margin:'0', padding:'0'}}>OSGD Assessment Platform</h3></div>
     <div className="App">
       <div className="grid">
-        <Question/>
+        <Question 
+          title="Addition of Two Numbers" 
+          objective="Complete the function solveMeFirst to compute the sum of two integers."
+          example={<div>a = 7<br/>b = 3<br/>Return 10.</div>}
+          functionDescription={
+            <div>
+              solveMeFirst has the following parameters: 
+              <ul>
+                <li>int a: the first value</li>
+                <li>int b: the second value</li>
+              </ul>
+              Returns<br/>
+              - int: the sum of a and b
+            </div>}
+          constraints={<div>1 &le; a,b &le; 1000</div>}
+          sampleInput={<div>a = 2<br/>
+            b = 3<br/></div>}
+          sampleOutput={<div>5</div>}
+          explanation={<div>2 + 3 = 5</div>}
+        />
         <Editor/>
       </div>
     </div>
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,50 +1,66 @@
 import React from 'react';
 import '../styles/Question.css';
 
-function Question() {
+interface QuestionParameters {
+    title: string;
+    objective: string;
+    example: JSX.Element;
+    functionDescription: JSX.Element;
+    constraints: JSX.Element;
+    sampleInput: JSX.Element;
+    sampleOutput: JSX.Element;
+    explanation: JSX.Element;
+}
+
+function Question(props: QuestionParameters) {
     return (
       <>
         <div className="question">
-          <h1>Addition of Two Numbers</h1>
-          <section id="objective">
-              <div>
-            <strong>Objective</strong><br/>
-            Complete the function solveMeFirst to compute the sum of two integers.
-            </div>
-          </section>
-          <section id="example">
-            <strong>Example</strong><br/>
-            a = 7<br/>
-            b = 3<br/>
-        Return 10.
-          </section>
-          <section id="function-description">
-              <strong>Function Description</strong><br/>
-              solveMeFirst has the following parameters:
-              <ul>
-                  <li>int a: the first value</li>
-                  <li>int b: the second value</li>
-              </ul>
-              Returns<br/>
-              - int: the sum of a and b
-          </section>
-          <section id="constraints">
-              <strong>Constraints</strong><br/>
-              1 &le; a,b &le; 1000
-          </section>
-          <section id="sample-input">
-              <strong>Sample Input</strong><br/>
-              a = 2<br/>
-              b = 3<br/>
-          </section>
-          <section id="sample-output">
-              <strong>Sample Output</strong><br/>
-              5
-          </section>
-          <section id="explanation">
-              <strong>Explanation</strong><br/>
-              2 + 3 = 5
-          </section>
+            {props.title && 
+                <h1>{props.title}</h1>
+            }
+            {props.objective && 
+                <section id="objective">
+                    <strong>Objective</strong><br/>
+                    {props.objective}
+                </section>
+            }
+            {props.example && 
+                <section id="example">
+                    <strong>Example</strong><br/>
+                    {props.example}
+                </section>
+            }
+            {props.functionDescription && 
+                <section id="function-description">
+                    <strong>Function Description</strong><br/>
+                    {props.functionDescription}
+                </section>
+            }
+            {props.constraints && 
+                <section id="constraints">
+                    <strong>Constraints</strong><br/>
+                    {props.constraints}
+                </section>
+            }
+            {props.sampleInput && 
+                <section id="sample-input">
+                    <strong>Sample Input</strong><br/>
+                    {props.sampleInput}
+                </section>
+            }
+            {props.sampleOutput && 
+                <section id="sample-output">
+                    <strong>Sample Output</strong><br/>
+                    5
+                </section>
+            }
+            {props.explanation && 
+                <section id="explanation">
+                    <strong>Explanation</strong><br/>
+                    2 + 3 = 5
+                </section>
+            }
         </div>
       </>
     )
